2767158d6c584c1fdd4cf4c9cebb732716fc3fef
JariBesonen <149715363+JariBesonen@users.noreply.github.com>
2025-11-08 08:24:34 -0700

    Setup Gamepage structure

--- src/Pages/Games.jsx
+++ src/Pages/Games.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-
+import "../Styles/Game.css";
 function Games() {
-
-    return(
-        <></>
-    )
+  return (
+    <div className="gamepage-wrapper">
+      <div className="game-wrapper">
+        <h2>game title</h2>
+        <div className="img-preview">img</div>
+        <button className="steam-link-btn">View on Steam</button>
+      </div>
+      <div className="game-wrapper">
+        <h2>game title</h2>
+        <div className="img-preview">img</div>
+        <button className="steam-link-btn">View on Steam</button>
+      </div>
+      <div className="game-wrapper">
+        <h2>game title</h2>
+        <div className="img-preview">img</div>
+        <button className="steam-link-btn">View on Steam</button>
+      </div>
+    </div>
+  );
 }
 
 export default Games;
