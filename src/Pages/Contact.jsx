7c994550926c66ebeac93555c4e6c9c597e703de
JariBesonen <149715363+JariBesonen@users.noreply.github.com>
2025-11-08 13:25:44 -0700

    Setup Contact page structure

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,9 +1,17 @@
 import React from "react";
-
+import '../Styles/Contact.css';
 function Contact() {
 
     return(
-        <></>
+        <div className="contact-page-wrapper">
+            <form className="contact-form">
+                <h2>Contact the Developer</h2>
+                <label htmlFor="email">your email address</label>
+                <input type="text" id="email" />
+                <button id="send-message-btn">send</button>
+            </form>
+            <h3>TPW</h3>
+        </div>
     )
 }
 
