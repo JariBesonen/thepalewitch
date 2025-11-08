d8c088c775cc64c5e055b07896a9e3929a156660
JariBesonen <149715363+JariBesonen@users.noreply.github.com>
2025-11-08 13:35:27 -0700

    Setup Support page structure

--- src/Pages/Support.jsx
+++ src/Pages/Support.jsx
@@ -1,9 +1,17 @@
 import React from "react";
-
+import '../Styles/Support.css';
 function Support() {
 
     return(
-        <></>
+        <div className="support-page-wrapper">
+            <form >
+                
+            </form>
+           <div className="about-wrapper">
+             <h2>Support the Developer</h2>
+            <span>description about the dev</span>
+           </div>
+        </div>
     )
 }
 
