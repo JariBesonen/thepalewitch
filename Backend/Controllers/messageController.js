6f28bc54c7414b698eb1ace6340bfad330c3dd41
JariBesonen <149715363+JariBesonen@users.noreply.github.com>
2025-12-14 11:52:13 -0700

    link username to message user created

--- Backend/Controllers/messageController.js
+++ Backend/Controllers/messageController.js
@@ -1,30 +1,48 @@
 const { postMessage, getMessage } = require("../Models/messageModel");
-
-
+const jwt = require("jsonwebtoken");
 const postMessageController = async (req, res) => {
-    const {message} = req.body;
-    if(!message) {
-        return res.status(400).json({error: "message is missing"});
+  const { message } = req.body;
+  const authorization = req.headers.authorization;
+  const token = authorization.split(" ")[1];
+  if (!token) {
+    return res.status(400).json({ error: "token is missing" });
+  }
+  console.log(token);
+  const decodedToken = jwt.decode(token);
+  const userId = decodedToken.id;
+  console.log(userId);
+  if (!userId) {
+    return res.status(400).json({ error: "token exists must id is missing" });
+  }
+
+  if (!message) {
+    return res.status(400).json({ error: "message is missing" });
+  }
+
+  try {
+    const newMessage = await postMessage(message, userId);
+
+    if (!newMessage) {
+      console.log("newMessage is missing");
+      return res.status(500).json({ error: "error with model" });
     }
-    try {
-        const newMessage = await postMessage(message);
-        return res.status(201).json(newMessage);
-    }catch(error) {
-        return res.status(500).json({error: 'error with messageController'});
-    }
-}
+    console.log(newMessage);
+    return res.status(201).json(newMessage);
+  } catch (error) {
+    return res.status(500).json({ error: "error with messageController" });
+  }
+};
 
 const getMessageController = async (req, res) => {
-try {
-const message = await getMessage();
-if(!message) {
-    return res.status(404).json({error: 'messages cannot be found'});
-}
-return res.status(200).json(message);
-}catch(error) {
-    return res.status(500).json({error: 'error with getMessageController'});
-}
-}
+  try {
+    const message = await getMessage();
+    if (!message) {
+      return res.status(404).json({ error: "messages cannot be found" });
+    }
+    return res.status(200).json(message);
+  } catch (error) {
+    return res.status(500).json({ error: "error with getMessageController" });
+  }
+};
 
-
-module.exports = {postMessageController, getMessageController};
+module.exports = { postMessageController, getMessageController };
--- Backend/Models/messageModel.js
+++ Backend/Models/messageModel.js
@@ -1,14 +1,17 @@
 const pool = require("../db.js");
 
-const postMessage = async (message) => {
-  const query = "INSERT INTO messages (message) VALUES ($1) RETURNING *";
-  const result = await pool.query(query, [message]);
+const postMessage = async (message, userId) => {
+  const query =
+    "INSERT INTO messages (message, userid) VALUES ($1, $2) RETURNING *";
+  const result = await pool.query(query, [message, userId]);
   return result.rows[0];
 };
 
 const getMessage = async () => {
-  const query = "SELECT * FROM messages LIMIT 30";
+  const query =
+    "SELECT users.username, messages.message, messages.messageid, messages.userid FROM messages INNER JOIN users ON users.id = messages.userid";
   const results = await pool.query(query);
+
   return results.rows;
 };
 
